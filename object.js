2526c1335eda0714c262ae7b2d0f9ea1e5b96c56
JIN SOL BAIK <121254493+KARIS-SOL@users.noreply.github.com>
2023-07-20 18:29:10 +0900

    forinforof

--- object.js
+++ object.js
@@ -28,3 +28,47 @@ delete ellie.hasJob;
 console.log(ellie.hasJob);
 
 // 2. Computed properties → 계산된 properties
+
+console.log(ellie.name);
+console.log(ellie["name"]);
+ellie["hasJob"] = true;
+console.log(ellie.hasJob);
+
+function printValue(obj, key) {
+  console.log(obj[key]);
+}
+printValue(ellie, "name");
+printValue(ellie, "age");
+
+// 3. Property value shorthand (Lovely shorthand)
+const person1 = { name: "bob", age: 2 };
+const person2 = { name: "steve", age: 3 };
+const person3 = { name: "dave", age: 4 };
+
+// function makePerson(name, age) {
+//   return {
+//     name,
+//     age,
+//   };
+// }
+
+// const person4 = makePerson("ellie", 30);
+// console.log(person4);
+
+// 4. Constructor Function
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const person4 = new Person("ellie", 30);
+console.log(person4);
+
+// 5. in operator: property existence check (key in obj)
+console.log("name" in ellie);
+console.log("age" in ellie);
+console.log("job" in ellie);
+console.log(ellie.job);
+
+// 6. for … in vs for … of
